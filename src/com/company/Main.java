05d9eade1ed028fc11b03fda4b594ff42b63fa27
kevin_xhk <kevinxhakaj@gmail.com>
2022-01-18 18:10:01 +0100

    "cleaned up Main"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -24,28 +24,24 @@ public class Main {
     public static void main(String[] args) {
         //TODO: MAKE PATH AS A COMMAND-LINE ARG
 	    //read csv
-        List<List<String>> lines = null;
-        Path path = Paths.get("/home/kev/Downloads/owid-covid-data.csv");
-
-        try(BufferedReader bf = Files.newBufferedReader(path)) {
-            lines = bf.lines()
-                    .map(x -> Arrays.asList(x.split(",", -1)))
-                    .toList();
-        }catch(Exception e) {
-            e.printStackTrace();
-        }
+        String filepath = "/home/kev/Downloads/owid-covid-data.csv";
+        List<List<String>> lines = getLines(filepath);
 
         //get column indices
         List<String> cols = lines.stream().findFirst().get();
 
-        //save indices needed by Country's constructor
+        //get list of countries
+        Map<Integer, Country> countries        = getCountries(lines, cols);
+
+        //get covid-reports
+        Map<Integer, CovidReport> covidReports = getCovidReports(lines,cols);
+
+    }
+
+    private static Map<Integer, CovidReport> getCovidReports(List<List<String>> lines, List<String> cols) {
+        //save indices needed by CovidReport's constructor
         int cod = cols.indexOf("iso_code");
         int cnt = cols.indexOf("continent");
-        int loc = cols.indexOf("location");
-        int pop = cols.indexOf("population");
-        int ma  = cols.indexOf("median_age");
-
-        //save indices needed by CovidReport's constructor
         int dt  = cols.indexOf("date");
         int tc  = cols.indexOf("total_cases");
         int nc  = cols.indexOf("new_cases");
@@ -58,24 +54,7 @@ public class Main {
         int tt  = cols.indexOf("total_tests");
         int si  = cols.indexOf("stringency_index");
 
-        //get list of countries
-        Map<Integer, Country> countries = lines.stream()
-                .skip(1)                                                //skip column names
-                .filter(distinctByKey(x -> x.get(cod)))                 //pick only 1 entry per iso_code
-                .filter(x -> !x.get(cnt).equals(""))                    //entries w/out continent are not countries
-                .map(x -> new Country (                                 //create a Country from List<String> fields
-                        x.get(cod),
-                        x.get(cnt),
-                        x.get(loc),
-                        x.get(pop),
-                        x.get(ma)))
-                .collect(Collectors.toMap(
-                        (x -> x.getIsocode().hashCode()), //make isocode + date as composite key
-                        Function.identity()));
-        System.out.println("COUNTRIES: " + countries.size());
-
-        //get covid-reports
-        Map<Integer, CovidReport> covidReports = lines.stream()
+        Map<Integer, CovidReport> output =                 lines.stream()
                 .skip(1)
                 .filter(x -> !x.get(cnt).equals(""))
                 .map(x -> new CovidReport(
@@ -94,6 +73,49 @@ public class Main {
                 .collect(Collectors.toMap(
                         (x -> (x.getIsocode() + x.getDate()).hashCode()), //make isocode + date as composite key
                         Function.identity()));
-        System.out.println("REPORTS: " + covidReports.size());
+        System.out.println("REPORTS: " + output.size());
+
+        return output;
+    }
+
+    private static Map<Integer, Country> getCountries(List<List<String>> lines, List<String> cols) {
+        //save indices needed by Country's constructor
+        int cod = cols.indexOf("iso_code");
+        int cnt = cols.indexOf("continent");
+        int loc = cols.indexOf("location");
+        int pop = cols.indexOf("population");
+        int ma  = cols.indexOf("median_age");
+
+        Map<Integer,Country> output = lines.stream()
+                .skip(1)                                                //skip column names
+                .filter(distinctByKey(x -> x.get(cod)))                 //pick only 1 entry per iso_code
+                .filter(x -> !x.get(cnt).equals(""))                    //entries w/out continent are not countries
+                .map(x -> new Country (                                 //create a Country from List<String> fields
+                        x.get(cod),
+                        x.get(cnt),
+                        x.get(loc),
+                        x.get(pop),
+                        x.get(ma)))
+                .collect(Collectors.toMap(
+                        (x -> x.getIsocode().hashCode()), //make isocode + date as composite key
+                        Function.identity()));
+        System.out.println("COUNTRIES: " + output.size());
+
+        return output;
+    }
+
+    private static List<List<String>> getLines(String filepath) {
+        Path path = Paths.get(filepath);
+        List<List<String>> output = null;
+
+        try(BufferedReader bf = Files.newBufferedReader(path)) {
+            output = bf.lines()
+                    .map(x -> Arrays.asList(x.split(",", -1)))
+                    .toList();
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        return output;
     }
 }
